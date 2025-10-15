3a120e58a966919df9041fe2af22843d42ce9b1c
tarit001 <tarit001@gmail.com>
2025-10-15 10:52:37 +0530

    update install.sh file

--- update.sh
+++ update.sh
@@ -27,5 +27,5 @@ echo "Starting Key loader.jar..."
 echo "Executing Burpsuite Professional..."
 echo "java --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -javaagent:$(pwd)/loader.jar -noverify -jar $(pwd)/burpsuite_pro_v$version.jar &" > burpsuitepro
 chmod +x burpsuitepro
-cp burpsuitepro /bin/burpsuitepro
+cp burpsuitepro /usr/bin/burpsuitepro
 (./burpsuitepro)
